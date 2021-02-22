7d636d64660997e8f2af465e095b0b25acfa4403
zarnocha <zarnocha00@gmail.com>
2021-02-22 17:51:37 +0100

    Zadanie5

---
+++ zad6/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("  //////\n");
+    printf(" +''''''+\n");
+    printf("(| o o |)\n");
+    printf("  | ^ |\n");
+    printf(" | '-' |\n");
+    printf(" +-----+\n");
+    return 0;
+}
+
+
+//6. Napisz program, który drukuje twarz podobn¹ do nastêpuj¹cej:
+/*
+   /////
+ +"""""+
+(| o o |)
+  | ^ |
+ | ‘-’ |
+ +-----+
+
+*/
