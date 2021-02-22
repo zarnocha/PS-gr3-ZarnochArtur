e42b5812bca402da6cc78ed4222f7ec59fa9f19e
zarnocha <zarnocha00@gmail.com>
2021-02-22 18:59:15 +0100

    Zadanie 9

---
+++ zad9/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+//9. Napisz program, który rysuje zwierzê mówi¹ce pozdrowienie. Zwierzê mo¿e byæ podobne (ale nie takie samo jak nastêpuj¹ce);
+
+int main()
+{
+    printf("                     ^  ^            ___\n");
+    printf(" (Pozdrawiam!)      +----+ ------+  /\n");
+    printf("             0OOo  | ^ ^ | | | |  |/\n");
+    printf("                O0 | =o= | / / /  |\n");
+    printf("                    +----+ ------+\n");
+    printf("                      | | |  | | |\n");
+    printf("                      |_|-+  |_|-+\n");
+    return 0;
+}
+
+/*
+
+                    /\ /\            ___
+   (Pozdrawiam!)    +----+ -------+ /
+                \  | ^ ^ |        |/
+                 \ | =o= |        |
+                    +----+ -------+
+                      | | |  | | |
+                      |_|-+  |_|-+
+
+    */
