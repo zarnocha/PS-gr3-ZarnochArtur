b6986b90281837eaa6a420302b4ffa868aeb8889
zarnocha <zarnocha00@gmail.com>
2021-02-22 19:16:03 +0100

    Zadanie 10

---
+++ zad10/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//10. Napisz program, który wypisuje w kolejnych liniach trzy pozycje, takie jak nazwiska Twoich trzech najlepszych przyjació³ lub trzech ulubionych filmów.
+/*
+>"Wyspa tajemnic",
+>"Pianista",
+>"Trauma".
+*/
+
+int main()
+{
+    printf("Trzy ulubione filmy to:\n>'Wyspa tajemnic',\n>'Pianista',\n>'Trauma'.");
+    return 0;
+}
