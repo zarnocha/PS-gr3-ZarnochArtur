01dfdbb2da7309ebaa0c1eec0e9def32abbf53b1
zarnocha <zarnocha00@gmail.com>
2021-02-22 19:31:52 +0100

    Zadanie 11

---
+++ zad11/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//11. Napisz program, który drukuje Twój ulubiony wiersz. Je¿eli nie masz ulubionego wiersza, poszukaj w Internecie jakiegokolwiek wiersza Wis³awy Szymborskiej.
+
+int main()
+{
+    printf("     Dobro i zlo\nZe zlem skrada sie sila\n       wladza\n  urzedowe twarze\nz dobrem przychodzi serce\n   chocby najmniejsze\n       jamnik\n   z kazda noga\nskrzywiona jak szczescie\nwiejskie na wsi Godzinki\n  gdy zmieniaja slowa\n  'niezwyciezonego\n plask w morde Samsona'");
+    printf("\n ");
+    return 0;
+}
