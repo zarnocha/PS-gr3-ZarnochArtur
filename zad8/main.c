5d3863bb91b3be13622a620e4ab32468a05840ab
zarnocha <zarnocha00@gmail.com>
2021-02-22 18:32:25 +0100

    Zadanie 8

---
+++ zad8/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*8. Napisz program, który rysuje dom, wygl¹daj¹cy dok³adnie tak jak poni¿ej:
+
+   +
+  + +
+ +   +
++-----+
+| .-. |
+| | | |
++-+-+-+
+*/
+int main()
+{
+    printf("   +\n");
+    printf("  + +\n");
+    printf(" +   +\n");
+    printf("+-----+\n");
+    printf("| .-. |\n");
+    printf("| | | |\n");
+    printf("+-+-+-+\n");
+    return 0;
+}
