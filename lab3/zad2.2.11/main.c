f72a47a692c5ba411031b554872225a067c17859
zarnocha <zarnocha00@gmail.com>
2021-03-15 13:36:59 +0100

    lab3

--- lab3/zad2.2.11/main.c
+++ lab3/zad2.2.11/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-float pierwiastek(int n)
+int pierwiastek(int n)
 {
     for(int i = 1; i<n; i++)
     {
@@ -10,14 +11,46 @@ float pierwiastek(int n)
             return i;
         }
     }
+    if(n == 1)
+    {
+        return 1;
+    }
     return -1;
 }
+
+int suma_calkowitych(int n)
+{
+    int suma = 0;
+    for(int i=0; i<=n; i++)
+    {
+        if(pierwiastek(i) != -1)
+        {
+            suma += sqrt(i);
+        }
+    }
+    return suma;
+}
+
+float suma_pierwiastkow(int n)
+{
+    float suma = 0;
+    for(int i=1; i<=n; i++)
+    {
+        if(pierwiastek(i) == -1)
+        {
+            suma += sqrt(i);
+        }
+    }
+    return suma;
+}
+
 int main()
 {
     int n;
     printf("Podaj nieujemna liczbe calkowita n: ");
     scanf("%d",&n);
     if(n<0) return -1;
-    printf("%f",pierwiastek(n));
+    float suma = suma_calkowitych(n) + suma_pierwiastkow(n);
+    printf("Suma pierwiastkow wynosi: %f",suma);
     return 0;
 }
