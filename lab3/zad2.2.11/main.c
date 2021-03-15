20a0e5c5b6dfafc1a1403324a8424bc81d64bbc2
zarnocha <zarnocha00@gmail.com>
2021-03-15 13:22:53 +0100

    lab3

--- lab3/zad2.2.11/main.c
+++ lab3/zad2.2.11/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float pierwiastek(int n)
+{
+    for(int i = 1; i<n; i++)
+    {
+        if(i*i == n)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int main()
 {
+    int n;
     printf("Podaj nieujemna liczbe calkowita n: ");
+    scanf("%d",&n);
+    if(n<0) return -1;
+    printf("%f",pierwiastek(n));
     return 0;
 }
