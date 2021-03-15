3540aaf9bbf14cfc04876db534da0d050ecdfe68
zarnocha <zarnocha00@gmail.com>
2021-03-15 12:45:55 +0100

    lab3

--- lab3/zad2.2.7/main.c
+++ lab3/zad2.2.7/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int potegowanie(int n, int m)
+{
+    int zmienna = m;
+    int wynik = 1;
+    while(zmienna > 0)
+    {
+        wynik *= n;
+        zmienna--;
+    }
+    return wynik;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    int m;
+    printf("Podaj n: ");
+    scanf("%d",&n);
+    printf("Podaj m: ");
+    scanf("%d",&m);
+    if(n == 0 && m == 0)
+    {
+        return -1;
+    }
+    int k = potegowanie(n,m);
+    printf("%d^%d = %d",n,m,k);
     return 0;
 }
