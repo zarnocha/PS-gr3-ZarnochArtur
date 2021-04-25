813498bae62b4df4f7e3b6964bdc493b01da8ecc
zarnocha <zarnocha00@gmail.com>
2021-04-25 23:19:24 +0200

    lab5

--- lab5/zad_1c/main.c
+++ lab5/zad_1c/main.c
@@ -21,10 +21,28 @@ int main()
     printf("Podaj liczbe elementow: ");
     int n;
     scanf("%d", &n);
+    int lewy;
+    int prawy;
 
-    if (n <= 0 || n > N)
+    printf("Podaj wartosc lewego: ");
+    scanf("%d", &lewy);
+    if(lewy < 0 || lewy > n)
     {
-        printf("Liczba elementów musi by´c z przedzia³u [1, %d\n]!", N);
+        printf("Lewy niepoprawny.");
+        return 1;
+    }
+
+    printf("Podaj wartosc prawego: ");
+    scanf("%d", &prawy);
+    if(prawy < 0 || prawy > n)
+    {
+        printf("Prawy niepoprawny.");
+        return 1;
+    }
+
+    if (n <= 0 || n > N )
+    {
+        printf("Liczba elementów musi byc z przedzialu [1, %d\n]!", N);
         return 1;
     }
 
@@ -37,7 +55,35 @@ int main()
         *p = rand() % lim + 1;
     }
 
-    wyswietl(tab, n);
+    printf("\n");
+    // Wypisuje n poczatkowych elementów tablicy
+    int i = 0;
+    for (short *p = tab; p < tab + n; ++p)
+    {
+        printf("%d. %5d \n",i,*p);
+        i++;
+    }
+    printf("\n");
 
+    short *l;
+    short *p;
+    int tmp;
+
+    for(l = tab + lewy, p = tab + prawy; l < tab + ((prawy- lewy+1) / 2) + lewy;)
+    {
+        tmp = *l;
+        *l = *p;
+        *p = tmp;
+        l++;
+        p--;
+    }
+        // Wypisuje n poczatkowych elementów tablicy
+    i = 0;
+    for (short *p = tab; p < tab + n; ++p)
+    {
+        printf("%d. %5d \n",i,*p);
+        i++;
+    }
+    printf("\n");
     return 0;
 }
