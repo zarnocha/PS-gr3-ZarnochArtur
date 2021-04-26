7ebd533aaaa725c161ab100595dccd453eba88fa
zarnocha <zarnocha00@gmail.com>
2021-04-26 14:46:00 +0200

    lab6

--- lab6/5.2.5/main.c
+++ lab6/5.2.5/main.c
@@ -11,27 +11,39 @@
 void przepisz(char napis1[], char napis2[])
 {
     int i;
-    for(i=0; i<sizeof(napis1); i++)
+    /*printf("Napis forem: ");
+    for(i=0; i < (sizeof(*napis1)/sizeof(char)); i++)
     {
-        napis2[i] = 0;
+        printf("%c",napis1[i]);
+    }*/
+    for(i=0; i < sizeof(napis1); i++)
+    {
         napis2[i] = napis1[i];
     }
 }
 
 int main()
 {
-    char napis1[] = "dlugi bardzo napis";
+    /*char napis1[] = "dlugi bardzo napis";
     printf("%s\n", napis1);
     printf("sizeof(napis1)%d\n", sizeof(napis1));
 
-    char napis2[(sizeof(napis1)*2)];
+    char napis2[(sizeof(napis1))];
 
-    printf("Napis2: %s\n", napis2);
+    printf("\nNapis2: %s", napis2);
     printf("sizeof(napis2)%d\n", sizeof(napis2));
 
-    przepisz(napis1, napis2);
+    przepisz(&napis1, &napis2);
 
-    printf("Napis2: %s", napis2);
+    printf("\n(Po zmianie) Napis2: %s", napis2);*/
+
+    char napis1[] = "napis";
+    char napis2[sizeof(napis1)];
+
+    printf("Napis2 undeclared: %s\n", napis2);
+
+    przepisz(napis1, napis2);
+    printf("Napis po zamianie: %s\n", napis2);
 
     return 0;
 }
