365548c85a5deb330a90d779bb4f0320e2a07031
zarnocha <zarnocha00@gmail.com>
2021-04-26 12:47:54 +0200

    lab6

---
+++ lab6/3.2.10/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Napisz bezargumentow¹ funkcjê, która rezerwuje pamiêæ
+// dla pojedynczej zmiennej
+// typu double i zwraca jako wartoœæ wskaŸnik do niej.
+
+int foo()
+{
+    return (double*)malloc(sizeof(double));
+}
+
+int main()
+{
+    //foo();
+    printf("%p", foo());
+    return 0;
+}
