2888cbd832ee64fb1e9f10df8254a194774724fc
zarnocha <zarnocha00@gmail.com>
2021-04-26 14:08:58 +0200

    lab6

---
+++ lab6/5.2.4/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int porownaj(char *nap1, char *nap2)
+{
+    int i;
+    for(i=0; (nap1[i] != 0) && (nap2[i] != 0); i++)
+        if(nap1[i] != nap2[i])
+            return (nap1[i] < nap2[i]) ?1:0;
+        if(nap1[i] != 0)
+            return 0;
+        else
+            return 1;
+}
+
+int main()
+{
+    char napis1[] = "abc";
+    char napis2[] = "bbb";
+
+    int porownanie = porownaj(napis1, napis2);
+
+    if(porownanie == 1)
+    {
+        printf("nap1[i] < nap2[i]");
+    }
+    else
+    {
+        printf("nap1[i] > nap2[i]");
+    }
+
+
+    return 0;
+}
