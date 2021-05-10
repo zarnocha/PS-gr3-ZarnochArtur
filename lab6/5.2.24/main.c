14db8f746e715e4e63049ccb958b2cc756578228
zarnocha <zarnocha00@gmail.com>
2021-05-10 21:56:07 +0200

    lab6

--- lab6/5.2.24/main.c
+++ lab6/5.2.24/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/*
+Napisz funkcjê kopiuj, która dostaje jako argumenty
+napis oraz tablicê znaków i przepisuje napis do
+otrzymanej w argumencie tablicy znaków
+*/
+
+void kopiuj(char *napis, char tablica[])
+{
+    for(int i=0; i<strlen(napis); i++)
+    {
+        tablica[i] = napis[i];
+    }
+    printf("\n***Kopiowanie zakonczone.***\n");
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    char *napis = "testowanie";
+    printf("Nap1: %s\n", napis);
+
+    char tablica[strlen(napis)];
+    printf("Nap2: %s\n", tablica);
+
+    size_t len1 = sizeof(napis)/sizeof(char);
+    size_t len2 = sizeof(tablica)/sizeof(char);
+
+    printf("\nlen1: %d\n", len1);
+    printf("len2: %d\n", len2);
+
+    kopiuj(napis, tablica);
+    printf("\nSkopiowany napis: %s\n", tablica);
+
     return 0;
 }
