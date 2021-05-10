078027f6aac5fd9ba2a54ef26671fb2deee47280
zarnocha <zarnocha00@gmail.com>
2021-05-10 15:00:40 +0200

    lab6

--- lab6/5.2.12/main.c
+++ lab6/5.2.12/main.c
@@ -1,59 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Napisz funkcjê wytnijzn, która dostaje jako argument dwa napisy
-nap1 i nap2, i wycina z napisu nap1 wszystkie znaki niewystêpuj¹ce
-w napisie nap2. Napisz dwie wersje funkcji wytnijzn dla napisów
-sk³adaj¹cych siê ze znaków typu char i wchar_t.
+/*Napisz funkcjÃª wytnijzn, ktÃ³ra dostaje jako argument dwa napisy
+nap1 i nap2, i wycina z napisu nap1 wszystkie znaki niewystÃªpujÂ¹ce
+w napisie nap2. Napisz dwie wersje funkcji wytnijzn dla napisÃ³w
+skÂ³adajÂ¹cych siÃª ze znakÃ³w typu char i wchar_t.
 */
 
-void wytnijzn(char nap1[], char nap2[], int len1, int len2)
+void wytnijzn(char nap1[], char nap2[], size_t len1, size_t len2)
 {
     char nap3[1000];
+
     int k = 0;
-    for(int i=0; i<len1; i++)
+
+    size_t len3 = sizeof(nap3) / sizeof(char);
+
+    for (int i = 0; i < len1; i++)
     {
-        for(int j=0; j<len2; j++)
+        for (int j = 0; j < len2; j++)
         {
-            if(nap1[i] == nap2[j])
+            if (nap1[i] != nap2[j])
+            {
+                continue;
+            }
+            else if (nap1[i] == nap2[j])
             {
                 nap3[k] = nap1[i];
                 k++;
-            }
-            else
-            {
                 break;
             }
         }
     }
-    nap1 = nap3;
-}
 
-void wytnijznak(char nap1[], char nap2[], int len1, int len2)
-{
-    char nap3[1000];
-    int k = 0;
-
-    size_t len3 = sizeof(nap3)/sizeof(char);
-    printf("\nlen3 %d\n", len3);
-
-    for(int i=0; i<len1; i++)
+    // printf("nap3: %s", nap3);
+    for (int i = 0; i < len3; i++)
     {
-        for(int j=0; j<len2; j++)
-        {
-            if(nap1[i] != nap2[j])
-            {
-                continue;
-            }
-            else if(nap1[i] == nap2[j])
-            {
-                nap3[k] = nap1[i];
-                k++;
-            }
-        }
+        nap1[i] = nap3[i];
     }
-
-    nap1[1000] = nap3[1000];
 
 }
 
@@ -64,20 +47,17 @@ int main()
     scanf("%s", nap1);
     printf("Nap1: %s\n", nap1);
 
-    printf("Podaj nap2: ");
-    char nap2[(sizeof(nap1))];
+    printf("\nPodaj nap2: ");
+    char nap2[1000];
     scanf("%s", nap2);
     printf("Nap2: %s\n", nap2);
 
-    size_t len1 = sizeof(nap1)/sizeof(char);
-    size_t len2 = sizeof(nap2)/sizeof(char);
+    size_t len1 = sizeof(nap1) / sizeof(char);
+    size_t len2 = sizeof(nap2) / sizeof(char);
+    len1 = sizeof(nap1);
 
-    printf("\nlen1: %d\n", len1);
-    printf("\nlen2: %d\n", len2);
+    wytnijzn(nap1, nap2, len1, len2);
 
-    wytnijznak(nap1, nap2, len1, len2);
-
-
-    printf("Nap1 po wytnijzn: %s\n", nap1);
+    printf("\nNap1 po wytnijzn: %s\n", nap1);
     return 0;
 }
