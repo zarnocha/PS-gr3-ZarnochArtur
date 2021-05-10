f155d37b20acc596ad3233de184b8e147764de78
zarnocha <zarnocha00@gmail.com>
2021-05-10 13:12:53 +0200

    lab6cmake

---
+++ lab6/cmake-build-debug/CMakeFiles/lab6.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/3.2.10/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/3.2.10/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/3.2.12/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/3.2.12/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/3.2.6/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/3.2.6/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.12/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.12/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.2/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.2/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.24/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.24/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.3/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.3/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.4/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.4/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.5/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.5/main.c.obj"
+  "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/5.2.6/main.c" "C:/GitHub/Programowanie strukturalne/PS-gr3-ZarnochArtur/lab6/cmake-build-debug/CMakeFiles/lab6.dir/5.2.6/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab6/cmake-build-debug/CMakeFiles/lab6.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6.dir/3.2.10/main.c.obj"
+  "CMakeFiles/lab6.dir/3.2.12/main.c.obj"
+  "CMakeFiles/lab6.dir/3.2.6/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.12/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.2/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.24/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.3/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.4/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.5/main.c.obj"
+  "CMakeFiles/lab6.dir/5.2.6/main.c.obj"
+  "lab6.exe"
+  "lab6.exe.manifest"
+  "lab6.lib"
+  "lab6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
