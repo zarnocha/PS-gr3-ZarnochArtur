f183a3ec48d2afe4c6ec408a17422417c64d5c22
zarnocha <zarnocha00@gmail.com>
2021-06-08 16:51:42 +0200

    Commit

--- Kolokwium2-ZarnochArtur/Zadanie_nr1.c
+++ Kolokwium2-ZarnochArtur/Zadanie_nr1.c
@@ -1,8 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 1024
+
+/*
+Zdefiniuj stałą MAX_LEN, a następnie zdefiniuj C-strukturę Produkt o polach nazwa typu char[MAX_LEN],
+cenaBrutto typu double oraz stawkaVAT typu float.
+
+Zdefiniuj następujące funkcje:
+
+Produkt makeProdukt(const char *nazwa, double cenaBrutto, float stawkaVAT) x
+
+Produkt makeProdukt1(const Produkt *produkt); x
+
+double cenaNetto(const Produkt *produkt);
+double kwotaVAT(const Produkt *produkt);
+void show(const Produkt *produkt); 			- nie mozna tym zrobic show Produktu bez *
+Napisz program testujący napisane funkcje.
+
+*/
+
+typedef struct Produkt{
+	char *nazwa;
+	double cenaBrutto;
+	float stawkaVAT;
+} Produkt;
+
+Produkt makeProdukt(const char *nazwa, double cenaBrutto, float stawkaVAT)
+{
+	Produkt temp;
+	temp.nazwa = (char*)malloc(MAX_LEN);
+	strcpy(temp.nazwa, nazwa);
+	temp.cenaBrutto = cenaBrutto;
+	temp.stawkaVAT = stawkaVAT;
+	return temp;
+}
+
+Produkt makeProdukt1(const Produkt *produkt)
+{
+	Produkt pr;
+	pr.nazwa = produkt->nazwa;
+	pr.cenaBrutto = produkt->cenaBrutto;
+	pr.stawkaVAT = produkt->stawkaVAT;
+	return pr;
+}
+
+void show(const Produkt *produkt)
+{
+	printf("\nNazwa: %s", produkt->nazwa);
+	printf("\nCena brutto: %0.2f", produkt->cenaBrutto);
+	printf("\nStawka VAT: %0.2f", produkt->stawkaVAT);
+}
+
+void show1(const Produkt produkt)
+{
+	printf("\nNazwa: %s", produkt.nazwa);
+	printf("\nCena brutto: %0.2f", produkt.cenaBrutto);
+	printf("\nStawka VAT: %0.2f", produkt.stawkaVAT);
+}
+
+double cenaNetto(const Produkt *produkt)
+{
+	return ((produkt->cenaBrutto) / (1+ (produkt->stawkaVAT/100)));
+}
+
+double kwotaVAT(const Produkt *produkt)
+{
+	return (produkt->cenaBrutto - cenaNetto(produkt));
+}
 
 int main()
 {
-    printf("Hello world!\n");
+	Produkt pr1 = makeProdukt("Produkt1", 66.70, 23);
+
+	Produkt *pr_temp = malloc(sizeof(Produkt));
+	pr_temp->nazwa = "Produkt2";
+	pr_temp->cenaBrutto = 75.20;
+	pr_temp->stawkaVAT = 11;
+
+	Produkt pr2 = makeProdukt1(pr_temp);
+
+	pr_temp->nazwa = "Produkt temp";
+
+	show1(pr1);
+	printf("\n");
+
+	show1(pr2);
+	printf("\n");
+
+	show(pr_temp);
+	printf("\n");
+
+	double Netto = cenaNetto(pr_temp);
+	double VAT = kwotaVAT(pr_temp);
+
+	printf("\nCena netto pr_temp: %f", Netto);
+	printf("\nKwota VAT pr_temp: %f\n", VAT);
+
+
     return 0;
 }
--- Kolokwium2-ZarnochArtur/Zadanie_nr2.c
+++ Kolokwium2-ZarnochArtur/Zadanie_nr2.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 500
+/*
+UWAGA! W poniższym programie do przechodzenia tablicy zamiast indeksów należy użyć wskaźników,
+a każdy operator indeksowania należy zastąpić operatorem dereferencji. Napisz program, który deklaruje stałą N=500,
+wczytuje do zmiennej n liczbę całkowitą z przedziału [1, N]
+i generuje n początkowych elementów tablicy o wartościach całkowitych z przedziału od -200 do 100,
+a następnie odwróci fragment tablicy przy pomocy funkcji odwróć
+
+    void odwroc(short tab[], int lewy, int prawy).
+
+Po wygenerowaniu tablicy oraz po każdej operacji należy wypisać zawartość tablicy.
+ Przetestuj odpowiednio program.
+*/
+
+void generacja_tablicy(short tab[], int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		*(tab+i) = -200 + rand() % (100 - (-200) + 1);
+	}
+}
+
+void show(short tab[], int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		printf("%d ", *(tab+i));
+	}
+}
 
 int main()
 {
-    printf("Hello world!\n");
+	int n;
+	printf("Podaj n: ");
+	scanf("%d", &n);
+
+	if(n<1)
+	{
+		printf("Za mala liczba.");
+		return 1;
+	}
+	else if(n>N)
+	{
+		printf("Za duza liczba.");
+		return 1;
+	}
+
+	short tab[N];
+	generacja_tablicy(tab, n);
+	show(tab, n);
     return 0;
 }
