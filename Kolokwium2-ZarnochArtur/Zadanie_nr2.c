52118f5a59ea76109a7e8363aa22106e3c383e96
zarnocha <zarnocha00@gmail.com>
2021-06-08 17:26:04 +0200

    Commit

--- Kolokwium2-ZarnochArtur/Zadanie_nr2.c
+++ Kolokwium2-ZarnochArtur/Zadanie_nr2.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #define N 500
+
 /*
-UWAGA! W poniższym programie do przechodzenia tablicy zamiast indeksów należy użyć wskaźników,
-a każdy operator indeksowania należy zastąpić operatorem dereferencji. Napisz program, który deklaruje stałą N=500,
-wczytuje do zmiennej n liczbę całkowitą z przedziału [1, N]
-i generuje n początkowych elementów tablicy o wartościach całkowitych z przedziału od -200 do 100,
-a następnie odwróci fragment tablicy przy pomocy funkcji odwróć
+UWAGA! W poni¿szym programie do przechodzenia tablicy zamiast indeksów nale¿y u¿yæ wskaŸników,
+a ka¿dy operator indeksowania nale¿y zast¹piæ operatorem dereferencji. Napisz program, który deklaruje sta³¹ N=500,
+wczytuje do zmiennej n liczbê ca³kowit¹ z przedzia³u [1, N]
+i generuje n pocz¹tkowych elementów tablicy o wartoœciach ca³kowitych z przedzia³u od -200 do 100,
+a nastêpnie odwróci fragment tablicy przy pomocy funkcji odwróæ
 
     void odwroc(short tab[], int lewy, int prawy).
 
-Po wygenerowaniu tablicy oraz po każdej operacji należy wypisać zawartość tablicy.
+Po wygenerowaniu tablicy oraz po ka¿dej operacji nale¿y wypisaæ zawartoœæ tablicy.
  Przetestuj odpowiednio program.
 */
 
 void generacja_tablicy(short tab[], int n)
 {
+	srand(time(NULL));
 	for(int i=0; i<n; i++)
 	{
 		*(tab+i) = -200 + rand() % (100 - (-200) + 1);
@@ -33,13 +37,21 @@ void show(short tab[], int n)
 void odwroc(short tab[], int lewy, int prawy)
 {
 	int temp;
-	int roznica = prawy-lewy+1;
+	int roznica = (prawy-lewy+1)/2;
 
-    for(int i = lewy, j = prawy; i < roznica; i++, j--)
+    /*for(int i = lewy, j = prawy; i < roznica; i++, j--)
     {
 		temp = *(tab+i);
 		*(tab+i) = *(tab+j);
 		*(tab+j) = temp;
+    }*/
+	prawy--;
+	lewy--;
+    for(int i = 0; i<roznica; i++)
+    {
+		temp = *(tab+lewy+i);
+		*(tab+lewy+i) = *(tab+prawy-i);
+		*(tab+prawy-i) = temp;
     }
 }
 
@@ -65,7 +77,7 @@ int main()
 	generacja_tablicy(tab, n);
 	show(tab, n);
 
-	odwroc(tab, 1, 5);
+	odwroc(tab, 3, 6);
 	printf("\n");
 	show(tab, n);
 
